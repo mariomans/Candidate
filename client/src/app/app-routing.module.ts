48ba1b14bc3668179ab2c15fbc53ef940c29c406
marios <frong_98_@hotmail.com>
2020-12-11 19:17:52 +0700

    Add login page to routing & module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ComponentComponent } from './component/component.component';
 import { UserEditComponent } from './user-edit/user-edit.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/user-list', pathMatch: 'full' },
+  {path: 'login', component: LoginComponent},
+  {path: '', component: LoginComponent},
+  {path: 'hello-world', component: HelloWorldComponent},
+  {path: 'logout', component: LoginComponent},
   {
     path: 'user-list',
     component: ComponentComponent
