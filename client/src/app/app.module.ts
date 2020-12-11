827d3c92e99672436e160c2b2bbc5df455601d93
marios <frong_98_@hotmail.com>
2020-12-11 22:42:55 +0700

    Bug fix version of module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentComponent } from './component/component.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatButton, MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { UserEditComponent } from './user-edit/user-edit.component';
+import { MenuComponent } from '../app/menu/menu.component';
+
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { HttpInterceptorService } from './httpInterceptor.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentComponent,
-    UserEditComponent
+    UserEditComponent,
+    MenuComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +37,11 @@ import { UserEditComponent } from './user-edit/user-edit.component';
     MatToolbarModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: HttpInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/menu/menu.component.ts
+++ client/src/app/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../login/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
